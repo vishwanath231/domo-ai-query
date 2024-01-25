1a0803c5178c3123ecc2ebf160a5997cfffa6c58
Vishwanath <vishwanatharuchunan@gmail.com>
2024-01-25 13:31:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,343 @@
+//   ___                  ___     _    _       
+//  |   \ ___ _ __  ___  | _ )_ _(_)__| |__ ___
+//  | |) / _ \ '  \/ _ \ | _ \ '_| / _| / /(_-<
+//  |___/\___/_|_|_\___/ |___/_| |_\__|_\_\/__/
+//
+// Visit https://developer.domo.com/portal/ec60c4980e1b5-getting-started-using-domo-bricks
+// for tips on getting started, linking to Domo data and debugging your app
+
+//Available globals
+var domo = window.domo; // For more on domo.js: https://developer.domo.com/docs/dev-studio-guides/domo-js#domo.get
+var datasets = window.datasets;
+
+var itemsPerPageOptions = [10, 25, 50, 100];
+var paginationSize = 10;
+var explanationWriteSpeed = 8; // the lower the faster, 0 is no delay
+
+var selectedDataSet = datasets[0];
+var sampleQuestions = [
+    {
+        question: "Show the first three rows",
+        sql: `SELECT * FROM ${selectedDataSet} LIMIT 3`,
+        explanation: `The SQL statement "SELECT * FROM ${selectedDataSet} LIMIT 3" is used to retrieve data from a table called "${selectedDataSet}" and limit the result to only the first 3 rows.\n\nExplanation:\n- "SELECT *" means that we want to select all columns from the table.\n- "FROM ${selectedDataSet}" specifies the table we want to retrieve data from, which is "${selectedDataSet}".\n- "LIMIT 3" limits the result to only the first 3 rows.\n\nErrors in the SQL query:\nThere are no apparent errors in the SQL query provided. However, it's worth noting that the query assumes the table "${selectedDataSet}" exists in the database. If the table name is incorrect or doesn't exist, an error will occur.`,
+    }
+];
+
+
+var questionForm = document.getElementById("questionForm");
+var questionInput = document.getElementById("questionInput");
+var dataTable = document.getElementById("dataTable");
+// var submitSqlButton = document.getElementById('submitButton');
+var explanationBlock = document.getElementById("explanationBlock");
+var explainSqlButton = document.getElementById("explainSqlButton");
+var explanationEl = document.getElementById("sqlExplanation");
+
+// Setup SQL Editor
+var editor = ace.edit("sqlStatement", {
+    mode: "ace/mode/sql",
+    selectionStyle: false,
+    theme: "ace/theme/github",
+    showPrintMargin: false,
+});
+
+// Setup events
+editor.on('change', clearExplanation);
+questionForm.addEventListener('submit', handleQuestionSubmit);
+explainSqlButton.addEventListener('click', handleExplainButtonClick);
+// submitSqlButton.addEventListener('click', handleSqlButtonClick);
+
+// Get the data
+var table, dataSourceSchema;
+Promise.all([
+    getDataSetSchema(selectedDataSet),
+    loadQuestion(selectedDataSet, 0),
+])
+    .then(handleResult);
+
+
+function handleResult(result) {
+    dataSourceSchema = result[0];
+    var data = result[1];
+
+    updateTable(data);
+}
+
+function loadQuestion(dataset, questionIndex) {
+    var sampleQuestion = sampleQuestions[questionIndex];
+    questionInput.value = sampleQuestion.question;
+    editor.setValue(sampleQuestion.sql);
+    editor.clearSelection();
+    if (sampleQuestion.explanation) {
+        setExplanation(sampleQuestion.explanation);
+    }
+
+    return domo.post(`/sql/v1/${dataset}`, sampleQuestion.sql, { contentType: 'text/plain' });
+}
+
+async function getDataSetSchema(dataSetAlias) {
+    var getRowQuery = `SELECT * from ${makeSafeText(dataSetAlias)} limit 1`; // the sql endpoint includes schema information we can use
+    try {
+        var singleRow = await domo.post(`/sql/v1/${makeSafeText(dataSetAlias)}`, getRowQuery, { contentType: 'text/plain' })
+
+        var dataSetSchemaColumns = singleRow.columns.map((column, index) => ({
+            name: column,
+            type: singleRow.metadata[index].type
+        }));
+
+        return {
+            dataSourceName: dataSetAlias,
+            description: "",
+            columns: dataSetSchemaColumns
+        };
+    }
+    catch (err) {
+        err.message = "Error: Unable to load DataSet Schema."
+        handleError(err);
+        return {};
+    }
+}
+
+function submitSQLQueryToDomo(sqlQuery) {
+    return domo.post(`/sql/v1/${datasets[0]}`, makeSafeText(sqlQuery), { contentType: 'text/plain' });
+}
+
+function getTableData(data) {
+    return data.rows.map(row => {
+        var obj = {};
+        data.columns.forEach((column, index) => {
+            obj[makeSafeText(column)] = makeSafeText(row[index]);
+        });
+        return obj;
+    });
+}
+
+function updateTable(data) {
+    if (table != null) {
+        table.destroy();
+    }
+
+    var options = {
+        data: getTableData(data),
+        layout: "fitDataFill",
+        autoColumns: true,
+    }
+    var showPagination = data && data.rows && data.rows.length > paginationSize;
+    if (showPagination) {
+        Object.assign(options, {
+            pagination: "local",
+            paginationSize: paginationSize,
+            paginationSizeSelector: itemsPerPageOptions,
+            paginationCounter: "rows",
+        });
+    }
+    table = new Tabulator(dataTable, options);
+}
+
+function toggleButtonSpinner(el, flag) {
+    if (flag === false || Array.from(el.classList).indexOf('loading') >= 0) {
+        el.classList.remove('loading');
+        if (el.dataset.prev) {
+            el.innerHTML = el.dataset.prev;
+        }
+    }
+    else {
+        el.classList.add('loading');
+        el.dataset.prev = el.innerHTML;
+        el.innerHTML =
+            `<div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>`;
+    }
+}
+
+async function handleQuestionSubmit(event) {
+    // Prevent the form from being submitted normally
+    event.preventDefault();
+
+    // Get the value of the question input field
+    var question = questionInput.value;
+
+    // Disable the submit button during loading
+    var submitButton = document.getElementById('generateSQLButton');
+    submitButton.disabled = true;
+    var prevLabel = submitButton.innerText;
+    submitButton.innerText = 'Generating SQL...';
+
+    try {
+        await handleFormSubmission(question, editor);
+        submitButton.disabled = false;
+        submitButton.innerText = prevLabel;
+    }
+    catch (err) {
+        submitButton.disabled = false;
+        submitButton.innerText = prevLabel;
+        err.message = "SQL generation failed. Please try again or adjust your question.";
+        handleError(err);
+    }
+
+    var sqlStatement = editor.getValue();
+    dataTable.classList.add('loading');
+
+    try {
+        var refreshedData = await submitSQLQueryToDomo(sqlStatement, selectedDataSet);
+        dataTable.classList.remove('loading');
+        updateTable(refreshedData);
+    }
+    catch (err) {
+        dataTable.classList.remove('loading');
+        err.message = "SQL Query failed. Check that your query doesn't have any typos or try the 'Explain SQL' button to try and diagnose the issue.";
+        handleError(err);
+    }
+}
+
+function clearExplanation() {
+    explanationEl.innerText = "";
+    explanationBlock.classList.add('empty');
+}
+function setExplanation(text) {
+    explanationBlock.classList.remove('empty');
+    explanationEl.innerText = "";
+    typeText(explanationEl, text);
+}
+
+async function handleExplainButtonClick(event) {
+    explainSqlButton.disabled = true;
+    setExplanation('Getting Explanation...');
+    toggleButtonSpinner(explainSqlButton, true);
+    var sqlStatement = editor.getValue();
+
+    try {
+        var sqlExplanation = await explainSql(sqlStatement);
+        explainSqlButton.disabled = false;
+        toggleButtonSpinner(explainSqlButton, true);
+        setExplanation(sqlExplanation);
+    }
+    catch (err) {
+        explainSqlButton.disabled = false;
+        toggleButtonSpinner(explainSqlButton, true);
+        err.message = "SQL Explanation failed. Please try again or adjust your query.";
+        handleError(err);
+    }
+}
+
+// async function handleSqlButtonClick(event) {
+//     submitSqlButton.disabled = true;
+//     var sqlStatement = editor.getValue();
+//     dataTable.classList.add('loading');
+//     toggleButtonSpinner(submitSqlButton, true);
+
+//     try {
+//         var refreshedData = await submitSQLQueryToDomo(sqlStatement, selectedDataSet);
+//         submitSqlButton.disabled = false;
+//         dataTable.classList.remove('loading');
+//         updateTable(refreshedData);
+//         toggleButtonSpinner(submitSqlButton, false);
+//     }
+//     catch (err) {
+//         submitSqlButton.disabled = false;
+//         dataTable.classList.remove('loading');
+//         toggleButtonSpinner(submitSqlButton, false);
+//         err.message = "SQL Query failed. Check that your query doesn't have any typos or try the 'Explain SQL' button to try and diagnose the issue.";
+//         handleError(err);
+//     }
+// }
+
+async function handleFormSubmission(question, sqlEditor) {
+    var sqlPrompt = await textToSql(question, dataSourceSchema);
+    var sqlStatement = sqlPrompt.choices[0].output;
+
+    // Display the SQL statement in the code editor
+    sqlEditor.setValue(sqlStatement);
+
+    return sqlStatement;
+}
+
+function textToSql(text, dataSourceSchema) {
+    var payload = {
+        input: text,
+        dataSourceSchemas: [dataSourceSchema]
+    };
+    return domo.post("domo/ai/v1/text/sql", payload);
+}
+
+async function explainSql(sql) {
+    var prompt = `Please (ELI5) the following sql statement to me: ${sql}. Please be as concise as possible and explain any errors that you find in the SQL query.`;
+    var endpoint = {
+        url: "generation",
+        body: {
+            "input": prompt
+        }
+    };
+
+    var sqlExplanation = await domo.post('/domo/ai/v1/text/' + endpoint.url, endpoint.body);
+    return sqlExplanation.choices[0] && sqlExplanation.choices[0].output;
+}
+
+function typeText(element, text, index = 0) {
+    var typeTextRecursive = function (element, text, index = 0) {
+        if (index < text.length) {
+            element.innerHTML = element.textContent + text.charAt(index);
+            setTimeout(() => typeText(element, text, index + 1), explanationWriteSpeed);
+        }
+    }
+    if (explanationWriteSpeed > 0) {
+        typeTextRecursive(element, makeSafeText(text), index);
+    }
+    else {
+        element.innerHTML = makeSafeText(text);
+    }
+}
+
+
+// Error Handling Functions
+function handleError(error) {
+    var message = error && error.message;
+    if (message && typeof message === 'string') {
+        if (message.toLowerCase() === 'forbidden') {
+            message = "Please contact ai@domo.com to request this feature be enabled in your instance."
+        }
+        else if (message.toLowerCase() === 'bad request') {
+            message = "Bad request. Please check the code submitting the request to ensure it looks correct and try again."
+        }
+    }
+    else { // If there is no message, assume the ai endpoint is disabled
+        message = "Please contact ai@domo.com to request this feature be enabled in your instance."
+    }
+    appendAlert(message);
+    console && console.warn && console.warn('Error: ' + message);
+}
+
+function appendAlert(message, hideIcon = true) {
+    var svg = hideIcon
+        ? ''
+        : '<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle"/></svg>';
+    var alert = `
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+      ${svg}
+      <span>
+        ${makeSafeText(message)}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </span>
+    </div>`;
+
+    var alertMessage = document.getElementById("alert-message");
+    alertMessage.innerHTML = alert;
+}
+
+
+// Create a safe version of an input value to be stored to the database
+// Transforms: "<h1>test</h1>"  =>  "&lt;h1&gt;test&lt;/h1&gt;"
+function makeSafeText(text) {
+    return String(text)
+        .replace(/&[/s]+/g, '&amp; ')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
+
+// Decode the text before displaying as an input value
+// Example: "&lt;h1&gt;test&lt;/h1&gt;"  =>  "<h1>test</h1>"
+function decodeSafeText(text) {
+    var div = document.createElement('div');
+    div.innerHTML = makeSafeText(text);
+    return div.innerText;
+}
